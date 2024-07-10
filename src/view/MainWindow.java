e8e8312a5024e447f4150645f48fc18cda53c47e
Claudio Coutinho <ccoutinho.ti@gmail.com>
2024-07-10 14:21:57 -0300

    change layout of main windows to BorderLayout

--- src/view/MainWindow.java
+++ src/view/MainWindow.java
@@ -24,7 +24,8 @@ public class MainWindow extends JFrame {
         this.startButton = new Button("Start");
         this.creditsButton = new Button("Credits");
 
-        this.setLayout(new FlowLayout(FlowLayout.LEFT)); // TODO: check new layouts, such as GridLayout
+//        this.setLayout(new FlowLayout(FlowLayout.LEFT)); // TODO: check new layouts, such as GridLayout
+        this.setLayout(new BorderLayout(10,10));
         this.buttonPanel.setLayout(new FlowLayout());
         this.imagePanel.setLayout(new FlowLayout());
 
@@ -39,7 +40,7 @@ public class MainWindow extends JFrame {
         this.add(buttonPanel);
 
         this.pack();
-//        this.setSize(new Dimension(300, 200));
+        this.setSize(new Dimension(300, 200));
         this.setDefaultCloseOperation(EXIT_ON_CLOSE);
         this.setLocationRelativeTo(null);
         this.setVisible(true);
